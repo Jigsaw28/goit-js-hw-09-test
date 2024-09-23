eddb73fe0e80268c150ff15bb113ccadea311a4b
Jigsaw28 <levenko7@gmail.com>
2024-09-23 21:36:39 +0300

    done all tasks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  let timeDelay = Number(formEl.delay.value);
+
+  for (let i = 1; i <= formEl.amount.value; i += 1) {
+    createPromise(i, timeDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    timeDelay += Number(formEl.step.value);
   }
 }
